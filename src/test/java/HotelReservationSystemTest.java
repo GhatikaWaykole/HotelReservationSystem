1a5ee467c9247a60575406e20bfe35c2776a47f5
ghatikawaykole <ghatika99@gmail.com>
2021-10-12 23:59:07 +0530

    Added method to compute the cheap hotel

--- src/test/java/HotelReservationSystemTest.java
+++ src/test/java/HotelReservationSystemTest.java
@@ -1,22 +1,25 @@
 import com.example.HotelReservation;
 import com.example.UserException;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.theories.suppliers.TestedOn;
 
 public class HotelReservationSystemTest {
     HotelReservation hotelReservation = new HotelReservation();
-
-    @Test
+    //Test method to check if hotel added properly
+    @Before
     public void CheckIfHotelAddedProperly() {
-        try {
-            hotelReservation.addHotel("BrideWood", 160);
             hotelReservation.addHotel("BridegWood", 160);
+            hotelReservation.addHotel("RidgeWood", 160);
             hotelReservation.addHotel("LakeWood", 120);
             int hotelCount = hotelReservation.hotelCount();
             Assert.assertEquals(3, hotelCount);
-        } catch (UserException e) {
-            e.printStackTrace();
-        }
+    }
+    //Test to check the cheapHotel data
+    @Test
+    public void givenDataHotel_WhenCheapPrice_ShouldReturnTrue(){
+        String cheapHotel = hotelReservation.cheapHotelName("12Sep2020","15Sep2020");
+        Assert.assertEquals("LakeWood",cheapHotel);
     }
 }
